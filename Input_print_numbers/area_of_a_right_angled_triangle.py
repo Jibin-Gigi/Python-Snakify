d6c3eb2b916961309c9354018dc344b580eaf09d
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-13 07:25:36 +0530

    Uploaded

---
+++ Input_print_numbers/area_of_a_right_angled_triangle.py
@@ -0,0 +1,6 @@
+#Read breadth and height as b,h 
+b = int(input())
+h = int(input())
+
+#Print area
+print(b*h/2)
---
+++ Input_print_numbers/sum_of_three_numbers.py
@@ -0,0 +1,7 @@
+#Reading 3 numbers
+a = int(input())
+b = int(input())
+c = int(input())
+
+#Printing Sum
+print(a + b + c)
