1f928aa173b246d89fed31bb3602d363c684651f
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-14 12:17:35 +0530

    Update replace_within_fragment.py

--- Strings/replace_within_fragment.py
+++ Strings/replace_within_fragment.py
@@ -4,5 +4,5 @@ Given a string. Replace every occurrence of the letter h by the letter H, except
 
 s = input()
 print(s[ :s.find('h')+1 ]+ 
-    s[s.find('h')+1 : s.rfind('h') ].replace('h','H') +
-    s[s.rfind('h') :])
+      s[s.find('h')+1 : s.rfind('h') ].replace('h','H') +
+      s[s.rfind('h') :])
