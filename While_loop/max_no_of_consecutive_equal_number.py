5b779063be06363ddc78621adc119fd8c78a3032
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-14 15:34:43 +0530

    Update max_no_of_consecutive_equal_number.py

--- While_loop/max_no_of_consecutive_equal_number.py
+++ While_loop/max_no_of_consecutive_equal_number.py
@@ -2,3 +2,18 @@
 Given a sequence of integer numbers ending with the number 0. 
 Determine the length of the widest fragment where all the elements are equal to each other.
 '''
+element = prev = int(input())
+count = 1
+max_count = 1
+while element != 0:
+    element = int(input())
+    if element == prev:
+        count += 1
+    else :
+        max_count = max(count,max_count)
+        prev = element
+        count = 1
+       
+    
+print(max_count)    
+    
