5f270b4bd7d23581875518baa922f0ff1b9105f8
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-17 12:42:21 +0530

    Update polyglots.py

--- Sets/polyglots.py
+++ Sets/polyglots.py
@@ -7,28 +7,28 @@
 
 
 # Read the number of students
-num_students = int(input())
+n = int(input())
 # Initialize sets for languages spoken by all students and at least one student
-all_languages = set()
-any_languages = set()
+all_langs = set()
+any_langs = set()
 
 # Loop through each student
-for _ in range(num_students):
+for _ in range(n):
     # Read the number of languages this student speaks
-    num_languages = int(input())
+    k = int(input())
     # Use a set comprehension to read the languages for this student
-    languages = {input() for _ in range(num_languages)}
+    langs = {input() for _ in range(k)}
     # Update the set of languages spoken by all students using set intersection
-    all_languages = languages if not all_languages else all_languages & languages
+    all_langs = langs if not all_langs else all_langs & langs
     # Update the set of languages spoken by at least one student using set union
-    any_languages |= languages
+    any_langs |= langs
 
 # Print the number of languages spoken by all students and the list of these languages
-print(len(all_languages))
-for language in sorted(all_languages):
-    print(language)
+print(len(all_langs))
+for lang in sorted(all_langs):
+    print(lang)
 
 # Print the number of languages spoken by at least one student and the list of these languages
-print(len(any_languages))
-for language in sorted(any_languages):
-    print(language)
+print(len(any_langs))
+for lang in sorted(any_langs):
+    print(lang)
