e123d2d1e68ecc38ba53860f34f6e3e719845007
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-20 10:33:27 +0530

    Update clock_face_1.py

--- Integer_and_float_numbers/clock_face_1.py
+++ Integer_and_float_numbers/clock_face_1.py
@@ -1,9 +1,9 @@
-'''
-H hours, M minutes and S seconds are passed since 
-the midnight (0 ≤ H < 12, 0 ≤ M < 60, 0 ≤ S < 60). 
-Determine the angle (in degrees) of the hour hand on the 
-clock face right now.
-'''
+''' H hours, M minutes and S seconds are passed since 
+    the midnight (0 ≤ H < 12, 0 ≤ M < 60, 0 ≤ S < 60). 
+    Determine the angle (in degrees) of the hour hand on the 
+    clock face right now. '''
+
+
 h=int(input())
 m=int(input())
 s=int(input())
