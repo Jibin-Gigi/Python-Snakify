8a1a84d63f4ff80e6e4005999e31711d9517a8fc
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2024-06-23 22:22:08 +0530

    Update sum_of_cubes.py

--- For_loop_with_range/sum_of_cubes.py
+++ For_loop_with_range/sum_of_cubes.py
@@ -1,7 +1,6 @@
-'''
-For the given integer N calculate the following sum:
-1^3+2^3+…+N^3
-'''
+''' For the given integer N calculate the following sum:
+1^3+2^3+…+N^3 '''
+
 
 n = int(input())
 result = 0
